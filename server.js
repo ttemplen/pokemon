b05f59b883915f48ffc5a70287fa2d0dd42b98d3
Todd Templen <trtemplen@gmail.com>
2021-01-26 18:33:25 -0600

    Commit 1 - My server is set up and running

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const pokemon = [
+    {
+      name: "Bulbasaur",
+      img: "http://img.pokemondb.net/artwork/bulbasaur.jpg"
+    },
+    {
+      name: "Ivysaur",
+      img: "http://img.pokemondb.net/artwork/ivysaur.jpg"
+    },
+    {
+      name: "Venusaur",
+      img: "http://img.pokemondb.net/artwork/venusaur.jpg"
+    },
+    {
+      name: "Charmander",
+      img: "http://img.pokemondb.net/artwork/charmander.jpg"
+    },
+    {
+      name: "Charizard",
+      img: "http://img.pokemondb.net/artwork/charizard.jpg"
+    },
+    {
+      name: "Squirtle",
+      img: "http://img.pokemondb.net/artwork/squirtle.jpg"
+    },
+    {
+      name: "Wartortle",
+      img: "http://img.pokemondb.net/artwork/wartortle.jpg"
+    }
+  ];
+app.get('/pokemon', (req, res) => {
+    res.send(pokemon);
+});
+app.get('/pokemon/:index', (req, res) => {
+    res.send(pokemon[req.params.index]);
+});
+app.listen(3000, ()=>{
+    console.log("I am listening");
+});
